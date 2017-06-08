a5a800a5f82d6289c6fdc3458a09917ac502aa78
comp9 <comp9@comp9_09.lab.unesc.net>
2017-06-07 21:57:07 -0300

    Realizando leitura de regras

--- src/com/linguagensformais/modelo/Producao.java
+++ src/com/linguagensformais/modelo/Producao.java
@@ -1,11 +1,16 @@
 package com.linguagensformais.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Producao {
 
     
     private String esquerda;
     private String direita;
+    private List<String> simbolosIsolados;
+    
     
 
     public String getEsquerda() {
@@ -24,6 +29,13 @@ public class Producao {
  
     public void setDireita(String direita) {
         this.direita = direita;
+    }
+    
+    private void isolarSimbolos(){
+        this.simbolosIsolados = new ArrayList<String>();
+        if(this.direita.charAt(0) == '\'' ){
+            String slice = this.direita.substring(direita.indexOf(esquerda) , 0);
+        }
     }
     
 }
